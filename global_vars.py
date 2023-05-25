5211f2a332700dad4403b38787b0d943d0432beb
Vishal Gattani <vishalgattani09@gmail.com>
2023-05-25 12:44:35 -0400

    global variables used

---
+++ global_vars.py
@@ -0,0 +1,12 @@
+
+min_charging_rate = 80.0
+max_charging_rate = 180.0
+decimal_places = 4
+verbose = False
+velocity = 105.0
+max_dist = 320.0
+earth_radius = 6356.752
+webscraper_data = True
+parse = False
+
+data_directory = "./data/"
