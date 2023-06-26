9c02a729dafc62b58faa6cb879dfc5beff4a1b10
Vishal Gattani <vishalgattani09@gmail.com>
2023-06-26 15:19:54 -0700

    optimized charging woohoo

--- node.py
+++ node.py
@@ -5,15 +5,42 @@ from global_vars import velocity ,max_dist, earth_radius
 def compute_drive_statistics(node1,node2):
 	if node1.id != node2.id:
 		dist = compute_distancetoreach(node1,node2)
-		if dist <= max_dist:
+		# assuming it has to full charge any way at the destination charging station
+		# if dist <= max_dist:
+		# 	node2.set_drivetimetoreach(dist/velocity)
+		# 	node2.set_distancetoreach(dist)
+		# 	timetocharge = dist/node2.get_charging_rate()
+		# 	node2.set_charge_time(timetocharge)
+		# else:
+		# 	node2.set_drivetimetoreach(float('inf'))
+		# 	node2.set_distancetoreach(float('inf'))
+		# 	timetocharge = float('inf')
+		# 	node2.set_charge_time(timetocharge)
+
+		if dist <= node1.remaining_fuel_distance:
+			if node1.charging_rate >= node2.charging_rate:
+				# charge at node 1 enough (full?) to reach node 2 so charging time at node 1 = 320.0 - node1.remaining_fuel_distance / charging rate at node 1
+				node2.set_drivetimetoreach(dist/velocity)
+				node2.set_distancetoreach(dist)
+				timetocharge = dist/node1.get_charging_rate()
+				node2.set_remaining_fuel_distance(node1.remaining_fuel_distance-dist)
+				node1.set_charge_time(timetocharge)
+			else:
+				# charge at node 2 so charging time at node 1 = 0
+				node2.set_drivetimetoreach(dist/velocity)
+				node2.set_distancetoreach(dist)
+				timetocharge = 0.0
+				node2.set_remaining_fuel_distance(node1.remaining_fuel_distance-dist)
+				node1.set_charge_time(timetocharge)
+		else:
+			# charge at node 1 enough to reach node 2 so charging time at node 1 = distance to node 2 / charging rate at node 1
 			node2.set_drivetimetoreach(dist/velocity)
 			node2.set_distancetoreach(dist)
-			timetocharge = dist/node2.get_charging_rate()
-		else:
-			node2.set_drivetimetoreach(float('inf'))
-			node2.set_distancetoreach(float('inf'))
-			timetocharge = float('inf')
-		node2.set_charge_time(timetocharge)
+			timetocharge = dist/node1.get_charging_rate()
+			node2.set_remaining_fuel_distance(node1.remaining_fuel_distance-dist)
+			node1.set_charge_time(timetocharge)
+
+
 
 def compute_distancetoreach(node1,node2,max_dist=max_dist):
 	return distance_on_earth_nodes(node1,node2)
@@ -25,7 +52,7 @@ def compute_heuristic(node,goal,velocity=velocity,max_dist=max_dist):
 	distance_from_goal = compute_distancetoreach(node,goal)
 	node.set_distancefromgoal(distance_from_goal)
 	if distance_from_goal !=0:
-		heuristic = node.get_drivetimetoreach() + node.get_charge_time() + node.get_distancefromgoal()/velocity + node.get_distancefromgoal()/134.0
+		heuristic = node.get_drivetimetoreach() + node.get_charge_time() + node.get_distancefromgoal()/velocity + 0.1*node.get_distancefromgoal()/node.get_charging_rate()
 	else: heuristic = node.get_drivetimetoreach() + node.get_charge_time()
 	heuristic = abs(heuristic)
 	node.set_heuristic(heuristic)
@@ -43,9 +70,16 @@ class Node(object):
 		self.parent = None
 		self.distancefromgoal = None
 		self.distancefromparent = float('inf')
+		self.remaining_fuel_distance = 0.0
 
 	# def __eq__(self, node: object) -> bool:
 	#     return self.id == node.id
+
+	def set_remaining_fuel_distance(self,remaining_fuel_distance):
+		self.remaining_fuel_distance = remaining_fuel_distance
+
+	def get_remaining_fuel_distance(self):
+		return self.remaining_fuel_distance
 
 	def get_details(self):
 		if self.parent:
