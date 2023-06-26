f911ff3ddba799b54e4d7d5976f52920886eb46a
Vishal Gattani <vishalgattani09@gmail.com>
2023-06-26 15:34:49 -0700

    clean up

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import warnings
 import chart_studio
 import chart_studio.plotly as cspy
 import chart_studio.tools as tls
-from queue import PriorityQueue
-
 
 from sample_data import network
 from tesla_supercharger_web_parser import tesla_supercharger_parser
@@ -25,10 +23,11 @@ if not os.path.exists(data_directory):
 chart_studio.tools.set_credentials_file(username=username, api_key=api_key)
 os.system('cls' if os.name == 'nt' else 'clear')
 
-
+# generate web-scraped dataset into a csv
 if parse:
 	tesla_supercharger_parser()
 
+# use web-scraped dataset
 if webscraper_data:
 	data = read_csv(data_directory+"supercharger-1.csv")
 	data.columns = ["Location","lat","lng"]
@@ -42,6 +41,7 @@ if webscraper_data:
 	start_node = Node(start.Location,start.lat,start.lng,start.charging_rate)
 	goal_node = Node(goal.Location,goal.lat,goal.lng,goal.charging_rate)
 
+# use sample network from coding challenge
 else:
 	network_df = pd.DataFrame(network,columns=["Location","lat","lng","charging_rate"])
 	network_df.to_csv(data_directory+"network.csv")
@@ -53,30 +53,29 @@ else:
 	start_node = Node(start.Location.values.tolist()[0],start.lat.values.tolist()[0],start.lng.values.tolist()[0],start.charging_rate.values.tolist()[0])
 	goal_node = Node(goal.Location.values.tolist()[0],goal.lat.values.tolist()[0],goal.lng.values.tolist()[0],goal.charging_rate.values.tolist()[0])
 
+# remove start_node
 query = data.copy()
 query = query.drop([start_idx])
 
+# create Node classes for each charging station
 nodes = []
 for i in range(len(query)):
 	row = query.iloc[i]
 	sample = Node(row.Location,row.lat,row.lng,row.charging_rate)
 	nodes.append(sample)
 
-# my planner
-
+# set the inital fuel charge to be the max distance it can travel
 start_node.set_remaining_fuel_distance(max_dist)
 
+# a-star planner with heuristics
 status = planner(start_node,goal_node,nodes)
 if isinstance(status, Node):
+    # generate backtracked path
 	path = backtrack(status)
+	# plot backtracked path using plotly
 	display_path(data,path)
 else:
+    # no path
 	path = None
 	warnings.warn("Planner couldn't find a path. Terminating...")
 	sys.exit()
-
-
-# new planner
-
-
-
